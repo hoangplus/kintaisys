1fbe51dcf37dc182d82f86968298e085fca6cd88
Hoang Nguyen <hoangquoc197@gmail.com>
2024-02-01 09:24:22 +0700

    [fix] update domain

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const nextConfig = {
     GOOGLE_CLIENT_ID:
       '22802419363-t0mgf4n0g9bl9todul47s62jo27hphe2.apps.googleusercontent.com',
     GOOGLE_CLIENT_SECRET: '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',
-    NEXTAUTH_URL: 'https://kintaisys.vercel.app/',
-    NEXTAUTH_URL_INTERNAL: 'https://kintaisys.vercel.app/',
+    NEXTAUTH_URL: 'https://kintaisys-eta.vercel.app/',
+    NEXTAUTH_URL_INTERNAL: 'https://kintaisys-eta.vercel.app/',
     NEXTAUTH_SECRET: 'c36cefe4d56653b1c615f4e0fbe580e3',
     NEXT_PUBLIC_API_URL: 'https://sheets.googleapis.com/v4/spreadsheets',
     SHEET_ID: '1QMqKfjVt4U_so4LyltUQaXbyOwupQuppIqkZ7hYH2yM',
