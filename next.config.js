13af61a2410e1fc362e29b67474032e279b01fa0
Hoang Nguyen <hoangquoc197@gmail.com>
2024-03-11 11:34:55 +0700

    [fix] deploy dev

--- next.config.js
+++ next.config.js
@@ -3,14 +3,14 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     GOOGLE_CLIENT_ID:
-      '96305492305-bout7jov9hgpl0uc9itmv8b7t86sc4ua.apps.googleusercontent.com',
-    GOOGLE_CLIENT_SECRET: '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',
-    NEXTAUTH_URL: 'http://localhost:3000/',
-    NEXTAUTH_URL_INTERNAL: 'http://localhost:3000/',
+      '22802419363-t0mgf4n0g9bl9todul47s62jo27hphe2.apps.googleusercontent.com',
+    GOOGLE_CLIENT_SECRET: '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',
+    NEXTAUTH_URL: 'https://kintaisys-eta.vercel.app/',
+    NEXTAUTH_URL_INTERNAL: 'https://kintaisys-eta.vercel.app/',
     NEXTAUTH_SECRET: 'c36cefe4d56653b1c615f4e0fbe580e3',
     NEXT_PUBLIC_API_URL: 'https://sheets.googleapis.com/v4/spreadsheets',
     SHEET_ID: '1QMqKfjVt4U_so4LyltUQaXbyOwupQuppIqkZ7hYH2yM',
-    API_KEY: 'AIzaSyCKVNGMGP904d8nCyPKPDvVfK-DekXlAhc',
+    API_KEY: 'AIzaSyAdvaYE_9MFrHkkP_tu5GdYVzVdO2No0ic',
   },
 };
 
